6078fa89c859e90db66a7428b6efe2c6bcc8ec96
Sun.Woo <842177252@qq.com>
2019-08-02 17:42:05 +0800

    1.0

---
+++ wuzs/test/src/Test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wuzhengsheng
+ * Date: 2019/8/2
+ * Time: 下午2:59
+ */
+
+namespace Wuzs\Test;
+
+class Test {
+
+    public function index()
+    {
+        echo 'Hello Wuzs_Test_Composer.....';
+    }
+}
